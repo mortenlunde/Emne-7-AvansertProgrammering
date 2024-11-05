3431f0187aa80cbde0f8b3e1bb53f8d9b7a6653f
Morten Lunde <morten.lunde@live.no>
2024-11-05 19:14:37 +0100

    file itsels x2

---
+++ StudentBlogg/Feature/Comments/CommentSearchParams.cs
@@ -0,0 +1,8 @@
+namespace StudentBlogg.Feature.Comments;
+
+public class CommentSearchParams
+{
+    public string Title { get; set; }
+    public string Content { get; set; } 
+    public DateTime DateCommented { get; set; }
+}
